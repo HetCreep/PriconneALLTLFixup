a7d445d51e960528ff00e5412d2a610935ba8a71
HetCreep <zxc59217412@gmail.com>
2026-04-27 15:53:08 +0700

    Update CoroutineStarter.cs

--- PriconneTLFixup/CoroutineStarter.cs
+++ PriconneTLFixup/CoroutineStarter.cs
@@ -1,36 +1,162 @@
-﻿using Il2CppInterop.Runtime.Injection;
+using BepInEx.Unity.IL2CPP.Utils.Collections;
+using Il2CppInterop.Runtime.Injection;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.CompilerServices;
 
-namespace PriconneTLFixup;
+namespace PriconneALLTLFixup;
 
 public class CoroutineStarter : MonoBehaviour
 {
+    #region 1. Thread-Safe Internal State
+
     private static CoroutineStarter? _instance;
-    public static Action? OnUpdate;
-    public static Action? OnInit;
+    private static readonly object _syncRoot = new();
+    private static int _mainThreadId;
+
+    private static readonly Queue<Action> _pendingQueue = new(32);
+    private static readonly List<Action> _executionBatch = new(32);
+
+    private static readonly Dictionary<float, WaitForSeconds> _waitCache = new(16);
+    #endregion
+
+    #region 2. Formal Properties & Events
+
+    public static event Action? OnFrameUpdate;
+    public static event Action? OnFrameLateUpdate;
+    public static event Action? OnFrameFixedUpdate;
+
+    public static bool IsMainThread => Environment.CurrentManagedThreadId == _mainThreadId;
+    #endregion
+
+    #region 3. Core Engine Initialization
+
+    public static void SetupMainThread()
+    {
+        if (_mainThreadId != 0) return;
+        _mainThreadId = Environment.CurrentManagedThreadId;
+        Log.Debug($"Core Thread ID defined: {_mainThreadId}");
+    }
 
     public static CoroutineStarter Instance
     {
         get
         {
-            if (_instance == null)
+            if (_instance != null) return _instance;
+            lock (_syncRoot)
             {
-                Log.Debug("Creating new CoroutineStarter");
-                ClassInjector.RegisterTypeInIl2Cpp<CoroutineStarter>();
-                var gameObject = new GameObject();
-                _instance = gameObject.AddComponent<CoroutineStarter>();
-                gameObject.name = typeof(CoroutineStarter).ToString();
-                DontDestroyOnLoad(gameObject);
-                OnInit?.Invoke();
-            }
+                if (_instance != null) return _instance;
 
-            var result = _instance;
-            return result;
+                if (_mainThreadId == 0) SetupMainThread();
+
+                if (!ClassInjector.IsTypeRegisteredInIl2Cpp<CoroutineStarter>())
+                    ClassInjector.RegisterTypeInIl2Cpp<CoroutineStarter>();
+
+                var container = new GameObject("PriconneTL_CoroutineCore");
+                container.hideFlags = HideFlags.HideAndDontSave;
+                DontDestroyOnLoad(container);
+
+                _instance = container.AddComponent<CoroutineStarter>();
+                Log.Info("[System] Coroutine Engine Started.");
+                return _instance;
+            }
         }
     }
-    
-    public void Update()
+
+    public CoroutineStarter(IntPtr ptr) : base(ptr) { }
+    #endregion
+
+    #region 4. Logic Flow: Unity Lifecycle
+
+    private void Update()
     {
-        OnUpdate?.Invoke();
+        if (_pendingQueue.Count > 0)
+        {
+            lock (_pendingQueue)
+            {
+                _executionBatch.Clear();
+                while (_pendingQueue.Count > 0)
+                    _executionBatch.Add(_pendingQueue.Dequeue());
+            }
+
+            for (int i = 0; i < _executionBatch.Count; i++)
+                InvokeSafe(_executionBatch[i]);
+        }
+
+        OnFrameUpdate?.Invoke();
     }
+
+    private void LateUpdate() => OnFrameLateUpdate?.Invoke();
+
+    private void FixedUpdate() => OnFrameFixedUpdate?.Invoke();
+
+    public void OnDestroy()
+    {
+        if (_instance == this) _instance = null;
+        _waitCache.Clear();
+        Log.Debug("CoroutineStarter engine stopped and memory released.");
+    }
+    #endregion
+
+    #region 5. Public API
+
+    public static void Dispatch(Action action)
+    {
+        if (action == null) return;
+
+        if (IsMainThread)
+        {
+            action();
+        }
+        else
+        {
+            lock (_pendingQueue) { _pendingQueue.Enqueue(action); }
+            _ = Instance;
+        }
+    }
+
+    public static Coroutine Run(IEnumerator routine)
+    {
+        if (routine == null) return null!;
+        return Instance.StartCoroutine(routine.WrapToIl2Cpp());
+    }
+
+    public static void Stop(Coroutine routine)
+    {
+        if (routine != null) Instance.StopCoroutine(routine);
+    }
+
+    public static void DelayExecute(float seconds, Action action)
+    {
+        if (action == null) return;
+        Run(DelayedRoutine(seconds, action));
+    }
+    #endregion
+
+    #region 6. Internal Robust Logic
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static void InvokeSafe(Action action)
+    {
+        try { action(); }
+        catch (Exception ex)
+        {
+            Log.Error($"[Coroutine] Task execution error: {ex.Message}");
+            Log.Debug(ex);
+        }
+    }
+
+    private static IEnumerator DelayedRoutine(float seconds, Action action)
+    {
+        if (!_waitCache.TryGetValue(seconds, out var wait))
+        {
+            wait = new WaitForSeconds(seconds);
+            _waitCache[seconds] = wait;
+        }
+        yield return wait;
+        InvokeSafe(action);
+    }
+    #endregion
 }
