7b8747de77ae444eb1643b0f638df728f6f210b7
HetCreep <zxc59217412@gmail.com>
2026-04-27 20:12:03 +0700

    Add files via upload

---
+++ PriconneALLTLFixup/WindowsAPI.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PriconneALLTLFixup;
+
+internal static class WindowsAPI
+{
+    [DllImport("user32.dll")]
+    public static extern IntPtr GetActiveWindow();
+
+    [DllImport("user32.dll", EntryPoint = "GetWindowLongPtr")]
+    private static extern IntPtr GetWindowLongPtr64(IntPtr hWnd, int nIndex);
+
+    [DllImport("user32.dll", EntryPoint = "GetWindowLong")]
+    private static extern int GetWindowLong32(IntPtr hWnd, int nIndex);
+
+    [DllImport("user32.dll", EntryPoint = "SetWindowLongPtr")]
+    private static extern IntPtr SetWindowLongPtr64(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+
+    [DllImport("user32.dll", EntryPoint = "SetWindowLong")]
+    private static extern int SetWindowLong32(IntPtr hWnd, int nIndex, int dwNewLong);
+
+    public static long GetWindowLong(IntPtr hWnd, int nIndex)
+    {
+        if (IntPtr.Size == 8) return (long)GetWindowLongPtr64(hWnd, nIndex);
+        return GetWindowLong32(hWnd, nIndex);
+    }
+
+    public static void SetWindowLong(IntPtr hWnd, int nIndex, long dwNewLong)
+    {
+        if (IntPtr.Size == 8) SetWindowLongPtr64(hWnd, nIndex, (IntPtr)dwNewLong);
+        else SetWindowLong32(hWnd, nIndex, (int)dwNewLong);
+    }
+}
